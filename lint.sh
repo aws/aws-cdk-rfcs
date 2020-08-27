1d8c216654c73c79132e2881a68b9ad075ef4cb4
epolon <epolon@amazon.com>
2020-08-27 16:07:28 +0300

    revert lint changes

--- lint.sh
+++ lint.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 linters=$PWD/tools/linters
 
 npm install --prefix $linters
-find . -type f -name '*.md' -not -path '*/node_modules/*' -not -path '*.history/*' -not -path '*/.github/*' |\
+find . -type f -name '*.md' -not -path '*/node_modules/*' -not -path '*/.github/*' |\
   xargs -I'{}' $linters/node_modules/.bin/markdownlint -c $linters/markdownlint.json $@ '{}'
