611dfb8090df7d8f30e4616fb02018604ac9a1d1
Adam Ruka <adamruka@amazon.com>
2021-03-16 18:08:15 -0700

    Revert the accidentally committed `lint.sh` changes.

--- lint.sh
+++ lint.sh
@@ -4,6 +4,6 @@
 set -euo pipefail
 linters=$PWD/tools/linters
 
-#npm install --prefix $linters
+npm install --prefix $linters
 find . -type f -name '*.md' -not -path '*/node_modules/*' -not -path '*/.github/*' |\
   xargs -I'{}' $linters/node_modules/.bin/markdownlint -c $linters/markdownlint.json $@ '{}'
